3882d4fdb665784e866022da4b9a38a0cf9110b0
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-02 01:42:51 +0900

    hard(2).c
    pointer learning!!

---
+++ hard(2).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  char str[50]="Unix Windows2000 Java C/C++ VB.Net";
+  char *sour, *dest, tmp[50]="";
+  sour = str;
+  dest = tmp;
+
+  printf("문자열 토큰 구분하기 \n");
+  while(*sour)
+  {
+    if(*sour ==32)
+    {
+      *dest++ = '\n';
+      sour++;
+    }
+    else
+    {
+      *dest++ = *sour++;
+    }
+  }
+  printf("%s \n",tmp);
+  return 0;
+}
