14f832c4774658ca2c6a55a766cafdda2cf26037
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-02 02:01:57 +0900

    Update hard(2).c

--- hard(2).c
+++ hard(2).c
@@ -3,24 +3,28 @@
 
 int main(void)
 {
-  char str[50]="Unix Windows2000 Java C/C++ VB.Net";
-  char *sour, *dest, tmp[50]="";
-  sour = str;
-  dest = tmp;
+	char str[50] = "Unix Windows2000 Java C/C++ VB.Net";
+	char* sour, * dest, tmp[50] = "";
+	sour = str;
+	dest = tmp;
 
-  printf("문자열 토큰 구분하기 \n");
-  while(*sour)
-  {
-    if(*sour ==32)
-    {
-      *dest++ = '\n';
-      sour++;
-    }
-    else
-    {
-      *dest++ = *sour++;
-    }
-  }
-  printf("%s \n",tmp);
-  return 0;
+	printf("문자열 토큰 구분하기 \n");
+	while (*sour)
+	{
+		if (*sour == 32)
+		{
+			printf("%s \n", tmp);
+			dest = tmp;
+			int num = strlen(tmp);
+			for (int i = 0; i < num; i++)
+				* dest++ = '\0';
+			dest = tmp;
+			sour++;
+		}
+		else
+		{
+			*dest++ = *sour++;
+		}
+	}
+	return 0;
 }
