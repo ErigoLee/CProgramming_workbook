a608125e018a7876d8649026befc1ebbf090ad16
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-02 21:15:02 +0900

    intermediate(3).c

---
+++ intermediate(3).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int x=10, y=20, tmp=30;
+
+  x++,++y;
+  tmp += x+y++;
+
+  printf("x: %d, y: %d, tmp: %d \n",x,y,tmp);
+  
+  return 0;
+}
+/*
+compiler
+x: 11, y: 22, tmp: 62
+*/
