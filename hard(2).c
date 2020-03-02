b28e7c217832476193eb5a7c9cb5ba74b99b1394
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-02 14:19:15 +0900

    hard(2).c

---
+++ hard(2).c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void stringToken(char* sour, char sep); //함수원형
+
+int main(void)
+{
+	char str[50] = "Unix Windows2000 Java C/C++ VB.Net";
+
+	stringToken(str, 32); //함수호출
+
+	return 0;
+}
+void stringToken(char* sour, char sep) //함구구현
+{
+	char tmp[50] = "", * dest;
+
+	dest = tmp;
+	printf("stringToken() 문자열 토큰 구분하기 \n");
+	while (*sour)
+	{
+		if (*sour == sep) {
+			printf("%s \n", tmp);
+			int size = sizeof(tmp);
+			dest = tmp;
+			for (int i = 0; i < size; i++)
+				* dest++ = '\0';
+			dest = tmp;
+			sour++;
+		}
+		else
+			*dest++ = *sour++;
+	}
+}
