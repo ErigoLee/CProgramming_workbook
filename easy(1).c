37b2876e53db0b3c91f689fcea91fb4845717d92
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-02 21:45:45 +0900

    easy(1).c

---
+++ easy(1).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int n=4;
+
+  switch(n)
+  {
+    case 1: n+=2;
+      break;
+    case 2: n*=2;
+      break;
+    case 3: n/=2;
+      break;
+  }
+  printf("n : %d \n",n);
+  return 0;
+}
+/*
+output outcome
+n: 4
+*/
