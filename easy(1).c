3fa0baf09cfb27d08424b57adfd24dcce95294b1
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-02 19:57:33 +0900

    easy(1).c

---
+++ easy(1).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  float height = 175.1;
+  int salary=3500000;
+
+  printf("성명: 홍길동 \n");
+  printf("키(신장): %f \n",height);
+  printf("월급: %d \n",salary);
+
+  return 0;
+}
