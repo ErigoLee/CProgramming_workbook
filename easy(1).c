1fd7553d3d1d309a0cf38a3c457140c465dc7d42
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-02 23:34:08 +0900

    easy(1).c

---
+++ easy(1).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+double salCalc(int sal);
+
+int main()
+{
+  int salary = 2500000;
+  double result;
+
+  result = salCalc(salary);
+  printf("월급: %d 의 5%% 성과급: %.2lf \n",salary,result);
+
+  return 0;
+}
+
+double salCalc(int sal)
+{
+  return sal*0.05;
+}
+/*
+output outcome
+월급: 2500000 의 5% 성과급: 125000.00
+*/
