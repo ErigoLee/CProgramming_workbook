4f9f9610b10bac0b9a4b692f57525da80af8f13d
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-03-03 23:47:20 +0900

    easy(1).c

---
+++ easy(1).c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+  char *ptr;
+
+  ptr = (char*)malloc(100);
+  if(ptr==NULL)
+  {
+    printf("메모리 할당 실패 !!! \n");
+    exit(1);
+  }
+
+  strcpy(ptr, "Hello ");
+  strcat(ptr, "world.");
+
+  printf("%d, %s \n",sizeof(ptr),ptr);
+
+  free(ptr);
+
+  return 0;
+  
+}
+/*
+compiler
+4, Hello world
+or
+8, Hello world
+
+4->32bit
+8->64bit
+*/
